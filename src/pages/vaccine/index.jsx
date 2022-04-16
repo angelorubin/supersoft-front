14e548de8cdafd6b8ebd92c6f876ee759ddd0b0e
Angelo Rubin <angelorubin@gmail.com>
2022-04-16 15:08:33 -0300

    Commit inicial

--- src/pages/vaccine/index.jsx
+++ src/pages/vaccine/index.jsx
@@ -156,7 +156,7 @@ export function Vaccine() {
 								sx={{
 									backgroundColor: "primary.main",
 									"&:hover": {
-										backgroundColor: "red",
+										backgroundColor: "primary",
 									},
 								}}
 							>
