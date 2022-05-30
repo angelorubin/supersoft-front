58bb10a9560d3e2137962ef1c65f6e57688ea906
angelorubin <angelorubin@gmail.com>
2022-05-29 23:10:37 -0300

    Implementando feature createVaccine

--- src/features/vaccine/Vaccine.jsx
+++ src/features/vaccine/Vaccine.jsx
@@ -1,256 +1,245 @@
-import { useState, useEffect, useMemo } from "react";
-import { Box, Typography, TextField, Button, Input } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, Typography, TextField, Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import {
-	createVaccine,
-	getVaccineById,
-	getVaccines,
-	searchKeyword,
-} from "features/vaccine/vaccineSlice";
-import {
-	increment,
-	decrement,
-	incrementAsync,
-} from "features/counter/counterSlice";
+import { createVaccine, getVaccines } from "features/vaccine/vaccineSlice";
 
 export function Vaccine() {
-	const dispatch = useDispatch();
-	const { status, data } = useSelector((state) => state.vaccine);
-	const [vaccines, setVaccines] = useState(data);
-	const { value } = useSelector((state) => state.counter);
-	const [open, setOpen] = useState(false);
-	const [keyword, setKeyword] = useState("");
-	const [vaccine, setVaccine] = useState({
-		nomeVacina: "",
-		nomeFabricante: "",
-		paisOrigem: "",
-		quantidadeMinimasDoses: "",
-		percentualEficaciaComprovada: "",
-		precoVendaPorDose: "",
-	});
+  const dispatch = useDispatch();
+  const { status, data } = useSelector((state) => state.vaccine);
+  const [vaccines, setVaccines] = useState(data);
+  const [keyword, setKeyword] = useState("");
+  const [vaccine, setVaccine] = useState({
+    nomeVacina: "",
+    nomeFabricante: "",
+    paisOrigem: "",
+    quantidadeMinimasDoses: "",
+    percentualEficaciaComprovada: "",
+    precoVendaPorDose: "",
+  });
 
-	useEffect(() => {
-		setVaccines(data);
-	}, []);
+  useEffect(() => {
+    dispatch(getVaccines());
+  }, [dispatch]);
 
-	useEffect(() => {
-		dispatch(getVaccines());
-	}, [dispatch]);
+  useEffect(() => {
+    setVaccines(data);
+  }, [data]);
 
-	const handleChange = (event) => {
-		const id = event.currentTarget.id;
-		const value = event.currentTarget.value;
+  useEffect(() => {}, []);
 
-		// console.log(id, value);
+  const handleChange = (event) => {
+    const id = event.currentTarget.id;
+    const value = event.currentTarget.value;
 
-		setVaccine((prevState) => {
-			return {
-				...prevState,
-				[id]: value,
-			};
-		});
-	};
+    // console.log(id, value);
 
-	const handleClick = () => {
-		dispatch(createVaccine(vaccine));
-		clearForm();
-	};
+    setVaccine((prevState) => {
+      return {
+        ...prevState,
+        [id]: value,
+      };
+    });
+  };
 
-	const clearForm = () => {
-		setVaccine(() => {
-			return {
-				nomeVacina: "",
-				nomeFabricante: "",
-				paisOrigem: "",
-				quantidadeMinimasDoses: "",
-				percentualEficaciaComprovada: "",
-				precoVendaPorDose: "",
-			};
-		});
-	};
+  const clearForm = () => {
+    setVaccine(() => {
+      return {
+        nomeVacina: "",
+        nomeFabricante: "",
+        paisOrigem: "",
+        quantidadeMinimasDoses: "",
+        percentualEficaciaComprovada: "",
+        precoVendaPorDose: "",
+      };
+    });
+  };
 
-	const handleIncrement = () => {
-		dispatch(increment());
-	};
+  const handleClickCreate = () => {
+    dispatch(createVaccine(vaccine));
+    dispatch(getVaccines());
+    clearForm();
+    setVaccines(data);
+  };
 
-	const handleDecrement = () => {
-		dispatch(decrement());
-	};
+  const handleChangeKeyword = (e) => {
+    setKeyword(e.target.value);
+  };
 
-	const handleChangeKeyword = (e) => {
-		setKeyword(e.target.value);
-	};
+  const handleClickSearch = (e) => {
+    const keywordToLowerCase = keyword.toLowerCase();
 
-	const handleClickSearch = () => {
-		const index = data.findIndex((vaccine) => +vaccine.id === +keyword);
+    const vaccine = data.filter((vaccine) => {
+      if (+vaccine.id === +keyword) {
+        return vaccine;
+      } else if (vaccine.nome_vacina.startsWith(keywordToLowerCase)) {
+        return vaccine;
+      } else if (vaccine.nome_fabricante.startsWith(keywordToLowerCase)) {
+        return vaccine;
+      }
+    });
 
-		if (index === -1) {
-			setVaccines(data);
-		} else {
-			setVaccines(() => {
-				return data[index];
-			});
-		}
-	};
+    if (vaccine.length > 0) {
+      setVaccines(vaccine);
+    } else {
+      setVaccines(data);
+    }
+  };
 
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				width: "100%",
-				alignItems: "center",
-			}}
-		>
-			<Box
-				sx={{
-					bgcolor: (theme) => theme.palette.common.black,
-					height: "10vh",
-					width: "100%",
-				}}
-			>
-				menu
-			</Box>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          bgcolor: (theme) => theme.palette.common.black,
+          height: "10vh",
+          width: "100%",
+        }}
+      >
+        menu
+      </Box>
 
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "column",
-					background: "#FFFFFF",
-					width: "85%",
-					marginTop: "30px",
-				}}
-			>
-				<Button sx={{ bgColor: "red" }} onClick={handleIncrement}>
-					+
-				</Button>
-				<Button onClick={handleDecrement}>-</Button>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          background: "#FFFFFF",
+          width: "85%",
+          marginTop: "30px",
+        }}
+      >
+        <Box sx={{ display: "flex", marginBottom: "1rem" }}>
+          <TextField
+            sx={{ width: "80%" }}
+            size="small"
+            value={keyword}
+            onChange={handleChangeKeyword}
+            placeholder="buscar pelo id, nome da vacina, nome do fabricante"
+          />
+          <Button onClick={handleClickSearch}>Buscar</Button>
+        </Box>
 
-				<Box sx={{ display: "flex", marginBottom: "0.5rem" }}>
-					<TextField
-						value={keyword}
-						onChange={handleChangeKeyword}
-						placeholder="busque pelo nome, id"
-					/>
-					<Button onClick={handleClickSearch}>Search</Button>
-				</Box>
+        <Box sx={{ display: "flex" }}>
+          <Typography
+            sx={{
+              fontFamily: "Roboto",
+              fontSize: "22px",
+              fontWeight: 700,
+              lineHeight: "26px",
+              letterSpacing: "0em",
+              textAlign: "left",
+            }}
+          >
+            Registrar Vacina
+          </Typography>
+        </Box>
 
-				<Box sx={{ display: "flex" }}>
-					<Typography
-						sx={{
-							fontFamily: "Roboto",
-							fontSize: "22px",
-							fontWeight: 700,
-							lineHeight: "26px",
-							letterSpacing: "0em",
-							textAlign: "left",
-						}}
-					>
-						Registrar Vacina
-					</Typography>
-				</Box>
+        <Box
+          sx={{
+            display: "flex",
+            marginTop: "30px",
+            gap: "1rem",
+            flexDirection: "column",
+          }}
+        >
+          <Box sx={{ display: "flex", gap: "1rem", flexDirection: "column" }}>
+            <TextField
+              fullWidth
+              size="small"
+              id="nomeVacina"
+              label="nome vacina"
+              type="text"
+              value={vaccine.nomeVacina}
+              onChange={handleChange}
+            />
 
-				<Box
-					sx={{
-						display: "flex",
-						marginTop: "30px",
-						gap: "1rem",
-						flexDirection: "column",
-					}}
-				>
-					<Box sx={{ display: "flex", gap: "1rem", flexDirection: "column" }}>
-						<TextField
-							fullWidth
-							size="small"
-							id="nomeVacina"
-							label="nome vacina"
-							type="text"
-							value={vaccine.nomeVacina}
-							onChange={handleChange}
-						/>
+            <TextField
+              fullWidth
+              size="small"
+              id="nomeFabricante"
+              label="fabricante"
+              type="text"
+              value={vaccine.nomeFabricante}
+              onChange={handleChange}
+            />
 
-						<TextField
-							fullWidth
-							size="small"
-							id="nomeFabricante"
-							label="fabricante"
-							type="text"
-							value={vaccine.nomeFabricante}
-							onChange={handleChange}
-						/>
+            <TextField
+              fullWidth
+              size="small"
+              id="paisOrigem"
+              label="país origem"
+              type="text"
+              value={vaccine.paisOrigem}
+              onChange={handleChange}
+            />
 
-						<TextField
-							fullWidth
-							size="small"
-							id="paisOrigem"
-							label="país origem"
-							type="text"
-							value={vaccine.paisOrigem}
-							onChange={handleChange}
-						/>
+            <TextField
+              fullWidth
+              size="small"
+              id="quantidadeMinimasDoses"
+              label="quantidades mínimas de doses"
+              type="text"
+              value={vaccine.quantidadeMinimasDoses}
+              onChange={handleChange}
+            />
 
-						<TextField
-							fullWidth
-							size="small"
-							id="quantidadeMinimasDoses"
-							label="quantidades mínimas de doses"
-							type="text"
-							value={vaccine.quantidadeMinimasDoses}
-							onChange={handleChange}
-						/>
+            <TextField
+              fullWidth
+              size="small"
+              id="percentualEficaciaComprovada"
+              label="percentual eficácia comprovada"
+              type="text"
+              value={vaccine.percentualEficaciaComprovada}
+              onChange={handleChange}
+            />
 
-						<TextField
-							fullWidth
-							size="small"
-							id="percentualEficaciaComprovada"
-							label="percentual eficácia comprovada"
-							type="text"
-							value={vaccine.percentualEficaciaComprovada}
-							onChange={handleChange}
-						/>
+            <TextField
+              fullWidth
+              size="small"
+              id="precoVendaPorDose"
+              label="preço venda dose"
+              type="text"
+              value={vaccine.precoVendaPorDose}
+              onChange={handleChange}
+            />
 
-						<TextField
-							fullWidth
-							size="small"
-							id="precoVendaPorDose"
-							label="preço venda dose"
-							type="text"
-							value={vaccine.precoVendaPorDose}
-							onChange={handleChange}
-						/>
+            <Button
+              size="small"
+              sx={{
+                backgroundColor: "primary.main",
+                color: "primary.contrastText",
+                "&:hover": {
+                  backgroundColor: "primary.light",
+                },
+              }}
+              onClick={handleClickCreate}
+            >
+              Registrar
+            </Button>
+          </Box>
 
-						<Button
-							size="small"
-							sx={{
-								backgroundColor: "primary.main",
-								color: "primary.contrastText",
-								"&:hover": {
-									backgroundColor: "primary.light",
-								},
-							}}
-							onClick={handleClick}
-						>
-							Registrar
-						</Button>
-					</Box>
-
-					<Box>
-						<Typography
-							sx={{
-								fontFamily: "Roboto",
-								fontSize: "22px",
-								fontWeight: 700,
-								lineHeight: "26px",
-								letterSpacing: "0em",
-								textAlign: "left",
-							}}
-						>
-							Vacinas registradas
-						</Typography>
-						<pre>{JSON.stringify(vaccines, null, 2)}</pre>
-					</Box>
-				</Box>
-			</Box>
-		</Box>
-	);
+          <Box>
+            <Typography
+              sx={{
+                fontFamily: "Roboto",
+                fontSize: "22px",
+                fontWeight: 700,
+                lineHeight: "26px",
+                letterSpacing: "0em",
+                textAlign: "left",
+              }}
+            >
+              Vacinas registradas
+            </Typography>
+            <pre>{JSON.stringify(vaccines, null, 2)}</pre>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
